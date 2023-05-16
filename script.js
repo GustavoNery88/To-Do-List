98cfd5792823299988c6ec2f6851d7080b70b92d
GustavoNery88 <gusta123sam@gmail.com>
2023-05-16 16:23:41 -0300

    tag de terminado

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let campoListaDeItens = document.getElementById("to-do-list");
+let inputCheck = document.getElementById("inputCheck")
 
 function novaTarefa() {
     let inputEntrada = document.getElementById("input-nova-tarefa");
@@ -29,20 +30,34 @@ function verificarTarefaRepetida(novaTarefa) {
     return false;
 }
 
-function atualizarLista() {
+function terminado(nome) {
+    const liElemento = document.querySelector(`li[data-nome="${nome}"]`);
+    liElemento.classList.toggle('terminar');
+    localStorage.setItem(`terminar-${nome}`, liElemento.classList.contains('terminar'));
+  }
+  
+  function atualizarLista() {
     let values = JSON.parse(localStorage.getItem("valoresGuardados") || '[]');
     localStorage.setItem("valoresGuardados", JSON.stringify(values));
     let listaHTML = "";
-
+  
     for (let i = 0; i < values.length; i++) {
-        listaHTML += `<li>${values[i].nome}  <button onclick='removerItem("${values[i].nome}")'> <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
-        <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
+      const tarefaNome = values[i].nome;
+      const tarefaTerminada = localStorage.getItem(`terminar-${tarefaNome}`) === 'true' ? 'terminar' : '';
+  
+      listaHTML += `<li data-nome="${tarefaNome}" class="${tarefaTerminada}"><input type="checkbox" onclick='terminado("${tarefaNome}")'> x </input>${tarefaNome} <button onclick='removerItem("${tarefaNome}")'> <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+      <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
+      <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
       </svg> </button></li>`;
     }
-
+  
+    const campoListaDeItens = document.getElementById("to-do-list");
     campoListaDeItens.innerHTML = listaHTML;
-}
+  }
+  
+  atualizarLista();
+  
+
 
 function removerItem(data) {
     let values = JSON.parse(localStorage.getItem("valoresGuardados") || '[]');
@@ -52,6 +67,4 @@ function removerItem(data) {
     atualizarLista();
 }
 
-
-// Chama a função atualizarLista ao carregar a página
 atualizarLista();
